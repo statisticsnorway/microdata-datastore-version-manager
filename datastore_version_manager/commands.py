71abfae472972176431624a8e53231c91fabd236
DanielElisenberg <daniel@elisenberg.com>
2021-12-16 17:11:58 +0100

    new_draft_to_datastore: update command

--- datastore_version_manager/commands.py
+++ datastore_version_manager/commands.py
@@ -3,32 +3,61 @@ import shutil
 from datastore_version_manager.adapter import (
     datastore, built_datasets
 )
-from datastore_version_manager.adapter.constants import USER_CHANGEABLE_RELEASE_STATUSES
+from datastore_version_manager.adapter.constants import (
+    USER_CHANGEABLE_RELEASE_STATUSES
+)
 from datastore_version_manager.domain import pending_operations
 
 
-def new_draft_to_datastore(dataset_name: str, description: str,
-                           operation: str):
-    built_dataset_path = built_datasets.get_dataset_path(dataset_name)
-    if not datastore.dataset_exists(dataset_name):
-        datastore.new_dataset_directory(dataset_name)
-    shutil.move(
-        f'{built_dataset_path}/{dataset_name}__0_0_0.json',
-        f'{datastore.get_metadata_dir_path(dataset_name)}/{dataset_name}__0_0_0.json'
+def add_new_dataset(dataset_name: str, description: str, overwrite: bool):
+    if datastore.is_dataset_in_data_store(dataset_name, "RELEASED"):
+        raise ForbiddenOperation(
+            f'Can not add new variable "{dataset_name}". '
+            'A versioned variable of the same name already exists in datastore'
+        )
+
+    release_status = pending_operations.get_release_status(dataset_name)
+
+    if release_status == 'DRAFT' and overwrite:
+        datastore.delete_draft_dataset(dataset_name)
+    elif release_status is not None:
+        raise ForbiddenOperation(
+            f'Can not add new variable "{dataset_name}"'
+            'It exists in the draft version of datastore with '
+            f'release status: {release_status}'
+        )
+
+    built_data_path = built_datasets.get_data_path(dataset_name)
+    if '.parquet' in built_data_path:
+        draft_data_path = (
+            f'{datastore.get_data_dir_path(dataset_name)}/'
+            f'{dataset_name}__0_0.parquet'
+        )
+    else:
+        draft_data_path = (
+            f'{datastore.get_data_dir_path(dataset_name)}/'
+            f'{dataset_name}__0_0'
+        )
+    built_metadata_path = built_datasets.get_metadata_path(dataset_name)
+    draft_metadata_path = (
+        f'{datastore.get_metadata_dir_path(dataset_name)}/'
+        f'{dataset_name}__0_0_0.json'
     )
-    shutil.move(
-        f'{built_dataset_path}/{dataset_name}__0_0.parquet',
-        f'{datastore.get_data_dir_path(dataset_name)}/{dataset_name}__0_0.parquet'
-    )
-    pending_operations.add_new(dataset_name, operation, "DRAFT", description)
+    shutil.move(built_metadata_path, draft_metadata_path)
+    shutil.move(built_data_path, draft_data_path)
+    pending_operations.add_new(dataset_name, "ADD", "DRAFT", description)
 
 
-def set_status(dataset_name: str, release_status: str, operation: str = None, description: str = None):
+def set_status(dataset_name: str, release_status: str, operation: str = None,
+               description: str = None):
     if release_status not in USER_CHANGEABLE_RELEASE_STATUSES['MUTABLE']:
         raise NoSuchReleaseStatus(
-            f'release status must be one of {USER_CHANGEABLE_RELEASE_STATUSES["MUTABLE"]}'
+            f'release status must be one of '
+            f'{USER_CHANGEABLE_RELEASE_STATUSES["MUTABLE"]}'
         )
-    pending_operations.set_release_status(dataset_name, release_status, operation, description)
+    pending_operations.set_release_status(
+        dataset_name, release_status, operation, description
+    )
 
 
 def hard_delete(dataset_name: str):
@@ -37,4 +66,8 @@ def hard_delete(dataset_name: str):
 
 
 class NoSuchReleaseStatus(Exception):
+    pass
+
+
+class ForbiddenOperation(Exception):
     pass
--- datastore_version_manager/domain/pending_operations.py
+++ datastore_version_manager/domain/pending_operations.py
@@ -1,6 +1,8 @@
 from datastore_version_manager.adapter import datastore
-from datastore_version_manager.adapter.constants import RELEASE_STATUS_ALLOWED_TRANSITIONS
 from datastore_version_manager.util import semver, date
+from datastore_version_manager.adapter.constants import (
+    RELEASE_STATUS_ALLOWED_TRANSITIONS
+)
 
 
 def add_new(dataset_name: str, operation: str, release_status: str,
@@ -18,14 +20,16 @@ def add_new(dataset_name: str, operation: str, release_status: str,
     pending_operations["version"] = semver.bump_draft_version(
         pending_operations["version"]
     )
-    
     datastore.write_pending_operations(pending_operations)
 
 
 def remove(dataset_name: str):
     pending_operations = datastore.get_pending_operations()
     datastructure_updates = pending_operations["dataStructureUpdates"]
-    if not any(dataset['name'] == dataset_name for dataset in datastructure_updates):
+    dataset_in_pending_operations = any(
+        dataset['name'] == dataset_name for dataset in datastructure_updates
+    )
+    if not dataset_in_pending_operations:
         raise datastore.DatasetNotFound(
             f'Dataset {dataset_name} not found in pending_operations.json'
         )
@@ -33,7 +37,6 @@ def remove(dataset_name: str):
         if datastructure_updates[i]['name'] == dataset_name:
             del datastructure_updates[i]
             break
-
     pending_operations["releaseTime"] = date.seconds_since_epoch()
     datastore.write_pending_operations(pending_operations)
 
@@ -72,14 +75,32 @@ def set_release_status(dataset_name: str, release_status: str, operation: str,
         # dataset not found -> it needs to be ADDED first
         else:
             raise DatasetNotFound(
-                f'Dataset {dataset_name} with status RELEASED not found in data_store'
+                f'Dataset {dataset_name} with status RELEASED '
+                'not found in data_store'
             )
 
 
+def get_release_status(dataset_name: str) -> str:
+    pending_operations = datastore.get_pending_operations()
+    data_structure_updates = pending_operations["dataStructureUpdates"]
+    try:
+        return next(
+            data_structure["releaseStatus"]
+            for data_structure in data_structure_updates
+            if data_structure["name"] == dataset_name
+        )
+    except StopIteration:
+        return None
+
+
 def __check_if_transition_allowed(old_release_status, new_release_status):
-    if new_release_status not in RELEASE_STATUS_ALLOWED_TRANSITIONS[old_release_status]:
+    allowed_transitions = (
+        RELEASE_STATUS_ALLOWED_TRANSITIONS[old_release_status]
+    )
+    if new_release_status not in allowed_transitions:
         raise ReleaseStatusTransitionNotAllowed(
-            f'Transition from {old_release_status} to {new_release_status} is not allowed'
+            f'Transition from {old_release_status} to {new_release_status} '
+            'is not allowed'
         )
 
 
