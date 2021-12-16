838d801206ac3aa98c50ec181b99d84f390d22e3
DanielElisenberg <daniel@elisenberg.com>
2021-12-16 17:11:03 +0100

    new_draft_to_datastore: updated adapters

--- datastore_version_manager/adapter/built_datasets.py
+++ datastore_version_manager/adapter/built_datasets.py
@@ -1,8 +1,35 @@
 import os
 
 
-def get_dataset_path(dataset_name: str) -> str:
-    dataset_dir = os.environ['BUILT_DATASETS_DIR']
-    for dataset_path in os.listdir(dataset_dir):
-        if dataset_path == dataset_name:
-            return f'{dataset_dir}/{dataset_name}'
+def get_metadata_path(dataset_name: str) -> str:
+    built_dataset_dir = os.environ['BUILT_DATASETS_DIR']
+    metadata_path = (
+        f'{built_dataset_dir}/{dataset_name}/{dataset_name}__0_0_0.json'
+    )
+    if os.path.exists(metadata_path):
+        return metadata_path
+    else:
+        raise NoBuiltDataset(
+            f"No built metadata file for {dataset_name}"
+        )
+
+
+def get_data_path(dataset_name: str) -> str:
+    built_dataset_dir = os.environ['BUILT_DATASETS_DIR']
+    partitioned_parquet_path = (
+        f'{built_dataset_dir}/{dataset_name}/{dataset_name}__0_0'
+    )
+    parquet_path = f'{partitioned_parquet_path}.parquet'
+
+    if os.path.exists(partitioned_parquet_path):
+        return partitioned_parquet_path
+    elif os.path.exists(parquet_path):
+        return parquet_path
+    else:
+        raise NoBuiltDataset(
+            f"No built data file for {dataset_name}"
+        )
+
+
+class NoBuiltDataset(Exception):
+    pass
