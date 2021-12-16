28c9837afc7450740a24f3599686d5b0a498e287
DanielElisenberg <daniel@elisenberg.com>
2021-12-16 17:12:37 +0100

    new_draft_to_datastore: update and new unit tests

--- tests/unit/domain/test_pending_operations.py
+++ tests/unit/domain/test_pending_operations.py
@@ -21,14 +21,20 @@ def teardown_function():
 
 @pytest.fixture(autouse=True)
 def setup_environment(monkeypatch):
-    monkeypatch.setenv('DATASTORE_ROOT_DIR', 'tests/resources/SSB_FDB')
-    monkeypatch.setenv('DATASET_BUILDER_OUTPUT_DIR', 'tests/resources/built_datasets')
+    monkeypatch.setenv(
+        'DATASTORE_ROOT_DIR', 'tests/resources/SSB_FDB'
+    )
+    monkeypatch.setenv(
+        'DATASET_BUILDER_OUTPUT_DIR', 'tests/resources/built_datasets'
+    )
 
 
 def test_remove_dataset_from_pending_operations():
     dataset_name = 'ANOTHER_TEST_DATASET'
     pending_operations.remove(dataset_name)
-    data_structure_updates = datastore.get_pending_operations()["dataStructureUpdates"]
+    data_structure_updates = (
+        datastore.get_pending_operations()["dataStructureUpdates"]
+    )
     assert len(data_structure_updates) == 1
     if datastore.draft_dataset_exists(dataset_name):
         assert False
@@ -37,4 +43,18 @@ def test_remove_dataset_from_pending_operations():
 def test_remove_non_existing_dataset_from_pending_operations():
     with pytest.raises(datastore.DatasetNotFound) as e:
         pending_operations.remove("DOES_NOT_EXIST")
-    assert "Dataset DOES_NOT_EXIST not found in pending_operations.json" in str(e.value)
+    assert (
+        "Dataset DOES_NOT_EXIST not found in pending_operations.json"
+     ) in str(e.value)
+
+
+def test_get_release_status():
+    dataset_name = 'ANOTHER_TEST_DATASET'
+    release_status = pending_operations.get_release_status(dataset_name)
+    assert release_status == 'DRAFT'
+
+
+def test_get_release_status_not_exist():
+    dataset_name = 'DOES_NOT_EXIST'
+    release_status = pending_operations.get_release_status(dataset_name)
+    assert release_status is None
--- tests/unit/test_datastore.py
+++ tests/unit/test_datastore.py
@@ -3,7 +3,8 @@ import os
 import pytest
 
 from datastore_version_manager.adapter import datastore
-from datastore_version_manager.adapter.datastore import DatasetNotFound
+
+DATASTORE_ROOT_DIR = 'tests/resources/SSB_FDB'
 
 
 def setup_function():
@@ -23,44 +24,50 @@ def teardown_function():
 
 @pytest.fixture(autouse=True)
 def setup_environment(monkeypatch):
-    monkeypatch.setenv('DATASTORE_ROOT_DIR', 'tests/resources/SSB_FDB')
-    monkeypatch.setenv('DATASET_BUILDER_OUTPUT_DIR', 'tests/resources/built_datasets')
+    monkeypatch.setenv('DATASTORE_ROOT_DIR', DATASTORE_ROOT_DIR)
+    monkeypatch.setenv(
+        'DATASET_BUILDER_OUTPUT_DIR', 'tests/resources/built_datasets'
+    )
 
 
 def test_draft_dataset_exists():
-    assert datastore.draft_dataset_exists('TEST_DATASET') == True
+    assert datastore.draft_dataset_exists('TEST_DATASET')
 
 
 def test_delete_draft_dataset_single_parquet():
     dataset_name = 'TEST_DATASET'
+    draft_data_path = (
+        f'{DATASTORE_ROOT_DIR}/data/{dataset_name}/'
+        f'{dataset_name}__0_0.parquet'
+    )
+    draft_metadata_path = (
+        f'{DATASTORE_ROOT_DIR}/metadata/{dataset_name}/'
+        f'{dataset_name}__0_0_0.json'
+    )
     datastore.delete_draft_dataset(dataset_name)
-    assert os.path.isdir(datastore.get_metadata_dir_path(dataset_name)) == False
-    assert os.path.isdir(datastore.get_data_dir_path(dataset_name)) == False
+    assert not os.path.exists(draft_data_path)
+    assert not os.path.exists(draft_metadata_path)
 
 
 def test_delete_draft_dataset_partitioned_parquet():
     dataset_name = 'TEST_DATASET_PARTITIONED'
+    draft_data_path = (
+        f'{DATASTORE_ROOT_DIR}/data/{dataset_name}/'
+        f'{dataset_name}__0_0'
+    )
+    draft_metadata_path = (
+        f'{DATASTORE_ROOT_DIR}/metadata/{dataset_name}/'
+        f'{dataset_name}__0_0_0.json'
+    )
     datastore.delete_draft_dataset(dataset_name)
-    assert os.path.isdir(datastore.get_metadata_dir_path(dataset_name)) == False
-    assert os.path.isdir(datastore.get_data_dir_path(dataset_name)) == False
-
-
-def test_remove_dataset_from_pending_operations():
-    dataset_name = 'TEST_DATASET'
-    datastore.remove_dataset_from_pending_operations(dataset_name)
-    pending_operations_list = datastore.get_pending_operations()["dataStructureUpdates"]
-    assert len(pending_operations_list) == 1
-    if datastore.draft_dataset_exists(dataset_name):
-        assert False
+    assert not os.path.exists(draft_data_path)
+    assert not os.path.exists(draft_metadata_path)
 
 
 def test_is_dataset_in_data_store():
-    assert datastore.is_dataset_in_data_store('SKATT_BRUTTOINNTEKT', 'RELEASED') == True
-    assert datastore.is_dataset_in_data_store('SKATT_BRUTTOINNTEKT', 'DELETED') == False
-
-
-def test_remove_non_existing_dataset_from_pending_operations():
-    try:
-        datastore.remove_dataset_from_pending_operations("DOES_NOT_EXIST")
-    except DatasetNotFound:
-        assert True
+    assert datastore.is_dataset_in_data_store(
+        'SKATT_BRUTTOINNTEKT', 'RELEASED'
+    )
+    assert not datastore.is_dataset_in_data_store(
+        'SKATT_BRUTTOINNTEKT', 'DELETED'
+    )
